cbe66fe010fe831d8ccbaa0ed74e40620d03d238
gcochard <greg.cochard@gmail.com>
2013-10-05 15:39:49 -0700

    Fixing contact form, changing from query param to email.

--- routes/send.js
+++ routes/send.js
@@ -1,13 +1,15 @@
+'use strict';
 var config = require('../modules/config')
   , app = config.app
   , nodemailer = require('nodemailer')
   , smtpTransport = nodemailer.createTransport("SMTP",{
-    service: "Gmail",
-    auth: {
-        user: "stephanieandgreg.us@gmail.com",
-        pass: "mailpasswordsneedtobelong"
+      service: "Gmail"
+    , auth: {
+          user: "stephanieandgreg.us@gmail.com"
+        , pass: "mailpasswordsneedtobelong"
     }
-  });
+  })
+  ;
 
 app.get('/contact',function(req,res){
   res.render('contact',{name:'stephanieandgreg.us - Contact',thanksdiv:'hidden',errordiv:'hidden'});
@@ -17,9 +19,19 @@ app.all('/send', function(req,res){
     console.log(req.body);
     var mailtext = '';
     if(req.body.name === undefined ||req.body.name === '' ||
-       req.body.from === undefined ||req.body.from === '' ||
+       req.body.email === undefined ||req.body.email === '' ||
        req.body.note === undefined ||req.body.note === ''){
-        res.redirect('/thanks?error=Please fill out all form fields');
+        var errormsg = [];
+        if(!req.body.name){
+          errormsg.push('name is empty');
+        }
+        if(!req.body.email){
+          errormsg.push('email is empty');
+        }
+        if(!req.body.note){
+          errormsg.push('note is empty');
+        }
+        res.redirect('/thanks?error=Please fill out all form fields, '+errormsg.join(', '));
     }else{
         Object.keys(req.body).forEach(function(key){
                 mailtext += key+': '+req.body[key]+'\n';
