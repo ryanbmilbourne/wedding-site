9c4db7d6fd8e337b26309cd713e2b89b641b60ae
gcochard <greg.cochard@gmail.com>
2013-10-09 09:22:48 -0700

    Update index.js
    Adding lots of config values.

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,9 @@ require('./manager');
 
 app.get('/', function(req,res){
   "use strict";
-  res.render('index',{name:'stephanieandgreg.us',
-    h1:'The wedding of Stephanie Schmidt and Greg Cochard',
-    date:'April 26, 2014',
-    loc:'<a href="https://maps.google.com/maps?ie=UTF8&amp;cid=15646201110297392647&amp;q=Five+Crowns&amp;iwloc=A&amp;gl=US&amp;hl=en">Five Crowns, 3801 East Coast Hwy, CA 92625</a>'
+  res.render('index',{name:config.NAME,
+    h1:config.EVENT_DESCRIPTION,
+    date:config.EVENT_DATE,
+    loc:config.EVENT_LOCATION_LINK
     });
 });
