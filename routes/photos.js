609d8df686dd4790e5b9e9acb6ebbabc422ae7b1
gcochard <greg.cochard@gmail.com>
2013-10-09 09:19:31 -0700

    Update photos.js
    adding placeholder for category of photos

--- routes/photos.js
+++ routes/photos.js
@@ -2,13 +2,14 @@ var config = require('../modules/config')
   , app = config.app
   , models = require('../models')
   , Photo = models.Photo
+  , category = 'yourcategory'
   ;
 
 app.get('/photos',function(req,res){
     "use strict";
-    Photo.find({category:'engagement',shown:true}).lean().sort({order:1}).exec(function(err,photos){
+    Photo.find({category:category,shown:true}).lean().sort({order:1}).exec(function(err,photos){
       res.render('photos',{
-          name:'stephanieandgreg.us - Photos',
+          name:config.NAME+' - Photos',
           photos: photos,
           errordiv:err?'':'hidden',
           error:err,
